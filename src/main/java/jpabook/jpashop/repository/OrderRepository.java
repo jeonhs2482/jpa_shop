7153d98ddc9f50c7cab0ddcb48377dfb610dea3e
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2025-03-04 11:15:19 +0900

    HomeController 및 html,css파일 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,5 +21,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // public List<Order> findAll(OrderSearch orderSearch) {}
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//        em.createQuery("select o from Order o join o.member m", Order.class);
+//    }
 }
